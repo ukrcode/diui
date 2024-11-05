767b77a8dc553b8ad7332cca5e9c6999238885a2
Stanislav Dolgachov <s.dolgachov@gmail.com>
2024-11-05 13:20:54 +0200

    feat: add and use e-ukraine font

--- packages/lib/postcss.config.js
+++ packages/lib/postcss.config.js
@@ -1,8 +1,23 @@
+const path = require("path");
+
+const sassPlugin = require("@csstools/postcss-sass");
+const assetsUrlPlugin = require("postcss-url");
+
+console.log(__dirname);
+
 const config = {
   plugins: [
-    require("@csstools/postcss-sass")({
+    sassPlugin({
       style: "expanded",
       sourceMap: true,
+    }),
+    assetsUrlPlugin({
+      url: "copy",
+      // base path to search assets from
+      basePath: path.resolve(__dirname, "fonts"),
+      // dir to copy assets
+      assetsPath: "fonts",
+      // using hash names for assets (generates from asset content)
     }),
   ],
   syntax: "postcss-scss",
