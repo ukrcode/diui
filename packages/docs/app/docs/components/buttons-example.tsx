b7bc62a9044a4cd2ffec653a84bf68b267aec756
Stanislav Dolgachov <s.dolgachov@gmail.com>
2024-11-25 12:58:42 +0200

    feat: add small buttons class

--- packages/docs/app/docs/components/buttons-example.tsx
+++ packages/docs/app/docs/components/buttons-example.tsx
@@ -1,11 +1,43 @@
+import "./buttons-example.css";
+
 export default function ButtonsExample() {
   return (
-    <div style={{display: "flex", gap: "1rem"}}>
-      <button className="di-btn">Regular button</button>
-      <button className="di-btn di-btn-primary">Primary</button>
-      <button className="di-btn di-btn-primary" disabled>Primary Disabled</button>
-      <button className="di-btn di-btn-outline">Outline</button>
-      <button className="di-btn di-btn-outline" disabled>Outline Disabled</button>
-    </div>
+    <>
+      <section>
+        <h4>Regular Buttons</h4>
+
+        <div className="buttons-example-row ">
+          <button className="di-btn">Regular button</button>
+          <button className="di-btn di-btn-primary">Primary</button>
+          <button className="di-btn di-btn-primary" disabled>
+            Primary Disabled
+          </button>
+          <button className="di-btn di-btn-outline">Outline</button>
+          <button className="di-btn di-btn-outline" disabled>
+            Outline Disabled
+          </button>
+        </div>
+      </section>
+
+      <section>
+        <h4>Regular Buttons</h4>
+
+        <div className="buttons-example-row ">
+          <button className="di-btn di-btn-sm">Regular button small</button>
+          <button className="di-btn di-btn-primary di-btn-sm">
+            Primary small
+          </button>
+          <button className="di-btn di-btn-primary di-btn-sm" disabled>
+            Primary Disabled small
+          </button>
+          <button className="di-btn di-btn-outline di-btn-sm">
+            Outline small
+          </button>
+          <button className="di-btn di-btn-outline di-btn-sm" disabled>
+            Outline Disabled small
+          </button>
+        </div>
+      </section>
+    </>
   );
 }
