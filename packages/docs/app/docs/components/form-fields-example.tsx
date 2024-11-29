884dced9940e66ff9b333c24a4f2c8dcbb4bd003
Stanislav Dolgachov <s.dolgachov@gmail.com>
2024-11-29 19:53:42 +0800

    feat: add animation to input

--- packages/docs/app/docs/components/form-fields-example.tsx
+++ packages/docs/app/docs/components/form-fields-example.tsx
@@ -1,10 +1,15 @@
 export default function FormFieldsExample() {
   return (
     <div className="di-form-field">
+      <input
+        type="text"
+        id="some-text-field"
+        placeholder=""
+        className="di-form-field-input"
+      />
       <label htmlFor="some-text-field" className="di-form-field-label">
         Label
       </label>
-      <input type="text" id="some-text-field" className="di-form-field-input" />
     </div>
   );
 }
