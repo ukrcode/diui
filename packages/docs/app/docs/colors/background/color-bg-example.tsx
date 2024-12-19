922989c426651d44802abc25abd7e231ac781a4f
Stanislav Dolgachov <s.dolgachov@gmail.com>
2024-12-19 23:25:55 +0200

    refactor: update background class names

--- packages/docs/app/docs/colors/background/color-bg-example.tsx
+++ packages/docs/app/docs/colors/background/color-bg-example.tsx
@@ -1,67 +1,67 @@
 export default function ColorBgExample() {
   return (
     <div>
-      <p className="di-gray-bg utils-p">
+      <p className="di-bg-gray utils-p">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, illo
         animi? Quaerat iure pariatur, quidem, molestias amet assumenda, porro
         minus illum fugiat sed omnis adipisci aliquid earum hic voluptate sunt!
       </p>
 
-      <p className="di-success-bg utils-p">
+      <p className="di-bg-success utils-p">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, illo
         animi? Quaerat iure pariatur, quidem, molestias amet assumenda, porro
         minus illum fugiat sed omnis adipisci aliquid earum hic voluptate sunt!
       </p>
 
-      <p className="di-warning-bg utils-p">
+      <p className="di-bg-warning utils-p">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, illo
         animi? Quaerat iure pariatur, quidem, molestias amet assumenda, porro
         minus illum fugiat sed omnis adipisci aliquid earum hic voluptate sunt!
       </p>
 
-      <p className="di-danger-bg utils-p">
+      <p className="di-bg-danger utils-p">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, illo
         animi? Quaerat iure pariatur, quidem, molestias amet assumenda, porro
         minus illum fugiat sed omnis adipisci aliquid earum hic voluptate sunt!
       </p>
 
-      <p className="di-info-bg utils-p">
+      <p className="di-bg-info utils-p">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, illo
         animi? Quaerat iure pariatur, quidem, molestias amet assumenda, porro
         minus illum fugiat sed omnis adipisci aliquid earum hic voluptate sunt!
       </p>
 
-      <p className="di-dodger-blue-bg utils-p">
+      <p className="di-bg-dodger-blue utils-p">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, illo
         animi? Quaerat iure pariatur, quidem, molestias amet assumenda, porro
         minus illum fugiat sed omnis adipisci aliquid earum hic voluptate sunt!
       </p>
 
-      <p className="di-violet-bg utils-p">
+      <p className="di-bg-violet utils-p">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, illo
         animi? Quaerat iure pariatur, quidem, molestias amet assumenda, porro
         minus illum fugiat sed omnis adipisci aliquid earum hic voluptate sunt!
       </p>
 
-      <p className="di-brick-red-bg utils-p">
+      <p className="di-bg-brick-red utils-p">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, illo
         animi? Quaerat iure pariatur, quidem, molestias amet assumenda, porro
         minus illum fugiat sed omnis adipisci aliquid earum hic voluptate sunt!
       </p>
 
-      <p className="di-pumpkin-bg utils-p">
+      <p className="di-bg-pumpkin utils-p">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, illo
         animi? Quaerat iure pariatur, quidem, molestias amet assumenda, porro
         minus illum fugiat sed omnis adipisci aliquid earum hic voluptate sunt!
       </p>
 
-      <p className="di-yellow-green-bg utils-p">
+      <p className="di-bg-yellow-green utils-p">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, illo
         animi? Quaerat iure pariatur, quidem, molestias amet assumenda, porro
         minus illum fugiat sed omnis adipisci aliquid earum hic voluptate sunt!
       </p>
 
-      <p className="di-lima-bg utils-p">
+      <p className="di-bg-lima utils-p">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, illo
         animi? Quaerat iure pariatur, quidem, molestias amet assumenda, porro
         minus illum fugiat sed omnis adipisci aliquid earum hic voluptate sunt!
