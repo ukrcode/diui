54fb9e01ae61638e2eec10bfd1f038725206395a
Stanislav Dolgachov <s.dolgachov@gmail.com>
2024-12-24 12:59:09 +0200

    refactor: move typography selector to separate file

--- packages/docs/app/page.tsx
+++ packages/docs/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         className={clsx(styles["heading-container"], "di-gr-bg-primary", "di-container")}
       >
         <h1 className="di-h1">🇺🇦 diui</h1>
-        <p className="di-body-text">
+        <p className="di-text-body">
           <span className="di-underline-md">неофіційна</span> CSS бібліотека
           побудована{" "}
           <span className="di-underline-md">на базі дизайн системи Дії</span>
