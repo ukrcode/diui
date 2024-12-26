702da5e040a604de84e1f9822f1970dd97bd4aa2
Stanislav Dolgachov <s.dolgachov@gmail.com>
2024-12-26 13:38:11 +0200

    feat: add business related badges

--- packages/docs/app/docs/components/badge/badge-example.tsx
+++ packages/docs/app/docs/components/badge/badge-example.tsx
@@ -1,3 +1,12 @@
 export default function BadgeExample() {
-  return <div className="di-badge di-badge-primary">Label</div>;
+  return (
+    <div className="di-flex di-gap-2">
+      <div className="di-badge di-badge-primary">Label Primary</div>
+      <div className="di-badge di-badge-secondary">Label Secondary</div>
+      <div className="di-badge di-badge-success">Label Success</div>
+      <div className="di-badge di-badge-warning">Label Warning</div>
+      <div className="di-badge di-badge-info">Label Info</div>
+      <div className="di-badge di-badge-danger">Label Danger</div>
+    </div>
+  );
 }
