bb85b16369fe6f7b17b027a9455bb958bb499bd1
Stanislav Dolgachov <s.dolgachov@gmail.com>
2024-12-28 19:45:30 +0200

    feat: add badges color example

--- packages/docs/app/docs/components/badge/badge-example.tsx
+++ packages/docs/app/docs/components/badge/badge-example.tsx
@@ -1,12 +1,30 @@
 export default function BadgeExample() {
   return (
-    <div className="di-flex di-gap-2">
-      <div className="di-badge di-badge-primary">Label Primary</div>
-      <div className="di-badge di-badge-secondary">Label Secondary</div>
-      <div className="di-badge di-badge-success">Label Success</div>
-      <div className="di-badge di-badge-warning">Label Warning</div>
-      <div className="di-badge di-badge-info">Label Info</div>
-      <div className="di-badge di-badge-danger">Label Danger</div>
+    <div className="di-flex di-flex-col di-gap-2">
+      <div className="di-flex di-gap-2">
+        <div className="di-badge di-badge-primary">Label Primary</div>
+        <div className="di-badge di-badge-secondary">Label Secondary</div>
+        <div className="di-badge di-badge-success">Label Success</div>
+        <div className="di-badge di-badge-warning">Label Warning</div>
+        <div className="di-badge di-badge-info">Label Info</div>
+        <div className="di-badge di-badge-danger">Label Danger</div>
+      </div>
+
+      <div className="di-flex di-gap-2">
+        <div className="di-badge di-badge-light-gray">Label Light Gray</div>
+        <div className="di-badge di-badge-gray">Label Gray</div>
+
+        <div className="di-badge di-badge-gorse">Label Gorse</div>
+        <div className="di-badge di-badge-yellow-green">Label Yellow Green</div>
+        <div className="di-badge di-badge-lima">Label Lima</div>
+        <div className="di-badge di-badge-malachite">Label malachite</div>
+        <div className="di-badge di-badge-radiance">Label radiance</div>
+        <div className="di-badge di-badge-dodger-blue">Label dodger blue</div>
+        <div className="di-badge di-badge-violet">Label violet</div>
+        <div className="di-badge di-badge-scarlet">Label scarlet</div>
+        <div className="di-badge di-badge-brick-red">Label brick red</div>
+        <div className="di-badge di-badge-pumpkin">Label pumpkin</div>
+      </div>
     </div>
   );
 }
