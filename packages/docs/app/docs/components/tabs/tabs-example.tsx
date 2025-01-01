f9d6506853d8fb92b3ce3d3d3024ad73133415c7
Stanislav Dolgachov <s.dolgachov@gmail.com>
2025-01-01 11:48:47 +0200

    docs: add tabs example

---
+++ packages/docs/app/docs/components/tabs/tabs-example.tsx
@@ -0,0 +1,49 @@
+export default function TabsExample() {
+  return (
+    <div className="di-tabs">
+      <ul className="di-tab-list" role="tablist">
+        <li>
+          <button
+            className="di-tab"
+            id="tab-1"
+            role="tab"
+            aria-controls="panel-1"
+            aria-selected="true"
+          >
+            Tab 1
+          </button>
+        </li>
+        <li>
+          <button
+            className="di-tab"
+            id="tab-2"
+            role="tab"
+            aria-controls="panel-2"
+            aria-selected="false"
+          >
+            Tab 2
+          </button>
+        </li>
+      </ul>
+
+      <div
+        className="di-tab-panel"
+        id="panel-1"
+        role="tabpanel"
+        aria-labelledby="tab-1"
+      >
+        Content for Tab 1
+      </div>
+
+      <div
+        className="di-tab-panel"
+        id="panel-2"
+        role="tabpanel"
+        aria-labelledby="tab-2"
+        hidden
+      >
+        Content for Tab 2
+      </div>
+    </div>
+  );
+}
