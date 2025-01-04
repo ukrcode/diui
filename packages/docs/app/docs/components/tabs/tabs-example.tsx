1cd125284360e2a9c491a5157b037d7a3f68a0aa
Stanislav Dolgachov <s.dolgachov@gmail.com>
2025-01-04 16:21:44 +0200

    feat: add button like tab

--- packages/docs/app/docs/components/tabs/tabs-example.tsx
+++ packages/docs/app/docs/components/tabs/tabs-example.tsx
@@ -1,49 +1,97 @@
 export default function TabsExample() {
   return (
-    <div className="di-tabs">
-      <ul className="di-tab-list" role="tablist">
-        <li>
-          <button
-            className="di-tab di-tab-active"
-            id="tab-1"
-            role="tab"
-            aria-controls="panel-1"
-            aria-selected="true"
-          >
-            Заголовок 1
-          </button>
-        </li>
-        <li>
-          <button
-            className="di-tab"
-            id="tab-2"
-            role="tab"
-            aria-controls="panel-2"
-            aria-selected="false"
-          >
-            Заголовок 2
-          </button>
-        </li>
-      </ul>
+    <section className="di-flex di-flex-column di-gap-2">
+      <div className="di-tabs">
+        <ul className="di-tab-list" role="tablist">
+          <li>
+            <button
+              className="di-tab di-tab-active"
+              id="tab-1"
+              role="tab"
+              aria-controls="panel-1"
+              aria-selected="true"
+            >
+              Заголовок 1
+            </button>
+          </li>
+          <li>
+            <button
+              className="di-tab"
+              id="tab-2"
+              role="tab"
+              aria-controls="panel-2"
+              aria-selected="false"
+            >
+              Заголовок 2
+            </button>
+          </li>
+        </ul>
 
-      <div
-        className="di-tab-panel"
-        id="panel-1"
-        role="tabpanel"
-        aria-labelledby="tab-1"
-      >
-        Content for Tab 1
+        <div
+          className="di-tab-panel"
+          id="panel-1"
+          role="tabpanel"
+          aria-labelledby="tab-1"
+        >
+          Content for Tab 1
+        </div>
+
+        <div
+          className="di-tab-panel"
+          id="panel-2"
+          role="tabpanel"
+          aria-labelledby="tab-2"
+          hidden
+        >
+          Content for Tab 2
+        </div>
       </div>
 
-      <div
-        className="di-tab-panel"
-        id="panel-2"
-        role="tabpanel"
-        aria-labelledby="tab-2"
-        hidden
-      >
-        Content for Tab 2
+      <div className="di-tabs">
+        <ul className="di-tab-list di-gap-2" role="tablist">
+          <li>
+            <button
+              className="di-tab-button di-tab-active"
+              id="tab-1"
+              role="tab"
+              aria-controls="panel-1"
+              aria-selected="true"
+            >
+              Заголовок 1
+            </button>
+          </li>
+          <li>
+            <button
+              className="di-tab-button"
+              id="tab-2"
+              role="tab"
+              aria-controls="panel-2"
+              aria-selected="false"
+            >
+              Заголовок 2
+            </button>
+          </li>
+        </ul>
+
+        <div
+          className="di-tab-panel"
+          id="panel-1"
+          role="tabpanel"
+          aria-labelledby="tab-1"
+        >
+          Content for Button Tab 1
+        </div>
+
+        <div
+          className="di-tab-panel"
+          id="panel-2"
+          role="tabpanel"
+          aria-labelledby="tab-2"
+          hidden
+        >
+          Content for Button Tab 2
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
