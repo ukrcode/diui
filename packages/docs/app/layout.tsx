79e0be8cd0d8ed667fa0c20032e8f584232e6c8d
Stanislav Dolgachov <stanislav_dolgachov@epam.com>
2025-01-09 15:07:01 +0200

    spike: add MUI to docs for sake of testing

--- packages/docs/app/layout.tsx
+++ packages/docs/app/layout.tsx
@@ -1,8 +1,12 @@
-import type { Metadata } from "next";
 import "./globals.css";
-
 // TODO switch to node_modules when finish development
 import "../../lib/css/diui.css";
+
+import type { Metadata } from "next";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../theme/theme";
 
 export const metadata: Metadata = {
   title: "diui CSS library documentation",
@@ -16,7 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="di-body di-page">{children}</body>
+      <body className="di-body di-page">
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
