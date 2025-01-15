6c92a2a3a8b41e6b8a81ded5c2514eec267ef2e6
Stanislav Dolgachov <s.dolgachov@gmail.com>
2025-01-15 12:59:22 +0200

    feat: add small checkbox

--- packages/docs/app/docs/components/form/form-check-example.tsx
+++ packages/docs/app/docs/components/form/form-check-example.tsx
@@ -1,9 +1,29 @@
 export default function FormCheckExample() {
-    return (
+  return (
+    <div className="di-flex di-gap-2">
       <div className="di-form-check">
-        <input id="form-check-id-1" className="di-form-check-input" type="checkbox" value="" />
-        <label htmlFor="form-check-id-1">Please confirm that you agree</label>
+        <input
+          id="form-check-id-1"
+          className="di-form-check-input"
+          type="checkbox"
+          value=""
+        />
+        <label className="di-form-check-label" htmlFor="form-check-id-1">
+          Please confirm that you agree
+        </label>
       </div>
-    );
-  }
-  
+
+      <div className="di-form-check di-form-check-sm">
+        <input
+          id="form-check-id-2"
+          className="di-form-check-input"
+          type="checkbox"
+          value=""
+        />
+        <label className="di-form-check-label" htmlFor="form-check-id-2">
+          Please confirm that you agree
+        </label>
+      </div>
+    </div>
+  );
+}
