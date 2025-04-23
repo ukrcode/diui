84d52fa69b054ed60701fc119974f9a6d3c6dced
Stanislav Dolgachov <s.dolgachov@gmail.com>
2025-04-23 19:26:05 +0300

    docs: add basic example for Card

---
+++ packages/docs/app/docs/components/card/card-example.tsx
@@ -0,0 +1,10 @@
+export default function CardExample() {
+    return (
+        <div className="di-flex di-flex-col di-gap-2">
+            <section className="di-card">
+                <h4>Card Title</h4>
+            </section>
+        </div>
+    );
+  }
+  
