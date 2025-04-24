3b7083eaeae8e1a6b52c739e9452e5aac2895bce
Stanislav Dolgachov <s.dolgachov@gmail.com>
2025-04-24 20:22:07 +0300

    feat: add basic styles for card

--- packages/docs/app/docs/components/card/card-example.tsx
+++ packages/docs/app/docs/components/card/card-example.tsx
@@ -1,10 +1,19 @@
 export default function CardExample() {
-    return (
-        <div className="di-flex di-flex-col di-gap-2">
-            <section className="di-card">
-                <h4>Card Title</h4>
-            </section>
+  return (
+    <div className="di-flex di-flex-col di-gap-2">
+      <article className="di-card">
+        <div className="di-card-inner">
+          <header>
+            <h4>Card Title</h4>
+          </header>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+            fringilla, nunc ut facilisis tincidunt, nisi erat venenatis ligula,
+            at convallis enim nunc nec nisi.
+          </p>
         </div>
-    );
-  }
-  
+      </article>
+    </div>
+  );
+}
