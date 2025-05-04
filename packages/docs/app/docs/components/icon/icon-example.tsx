ce3c1532f78bae1aaeebabc7c1c4d0c423bb1bcd
Stanislav Dolgachov <s.dolgachov@gmail.com>
2025-05-04 21:21:40 +0300

    feat: add initial icon feature

---
+++ packages/docs/app/docs/components/icon/icon-example.tsx
@@ -0,0 +1,8 @@
+
+export default function IconExample() {
+    return (
+        <div>
+            <span className="di-icon di-icon-file" role="img" aria-label="File Icon"></span>
+        </div>
+    );
+}
