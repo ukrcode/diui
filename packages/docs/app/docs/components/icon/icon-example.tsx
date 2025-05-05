2be293fe7941b0749aa9fa02c9ee192d289a1f32
Stanislav Dolgachov <s.dolgachov@gmail.com>
2025-05-05 12:27:35 +0300

    feat: add icon sizes

--- packages/docs/app/docs/components/icon/icon-example.tsx
+++ packages/docs/app/docs/components/icon/icon-example.tsx
@@ -1,8 +1,50 @@
+function IconAllSizesExample({ iconName }: { iconName: string }) {
+  return (
+    <div style={{ display: "flex", alignItems: "center", marginBottom: "1rem" }}>
+      <span
+        className={`di-icon di-icon-${iconName} di-icon-64`}
+        role="img"
+        aria-label="File Icon"
+      ></span>
+      <span
+        className={`di-icon di-icon-${iconName} di-icon-56`}
+        role="img"
+        aria-label="File Icon"
+      ></span>
+      <span
+        className={`di-icon di-icon-${iconName} di-icon-48`}
+        role="img"
+        aria-label="File Icon"
+      ></span>
+      <span
+        className={`di-icon di-icon-${iconName} di-icon-40`}
+        role="img"
+        aria-label="File Icon"
+      ></span>
+      <span
+        className={`di-icon di-icon-${iconName} di-icon-32`}
+        role="img"
+        aria-label="File Icon"
+      ></span>
+      <span
+        className={`di-icon di-icon-${iconName} di-icon-24`}
+        role="img"
+        aria-label="File Icon"
+      ></span>
+      <span
+        className={`di-icon di-icon-${iconName}`}
+        role="img"
+        aria-label="File Icon"
+      ></span>
+    </div>
+  );
+}
 
 export default function IconExample() {
-    return (
-        <div>
-            <span className="di-icon di-icon-file" role="img" aria-label="File Icon"></span>
-        </div>
-    );
+  return (
+    <>
+      <IconAllSizesExample iconName="file" />
+      <IconAllSizesExample iconName="download" />
+    </>
+  );
 }
