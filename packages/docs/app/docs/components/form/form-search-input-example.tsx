c342b97483ae98ef1a3a4a264658d1f1675ee2f5
Stanislav Dolgachov <s.dolgachov@gmail.com>
2025-05-08 23:20:46 +0300

    chore: move icon inside label

--- packages/docs/app/docs/components/form/form-search-input-example.tsx
+++ packages/docs/app/docs/components/form/form-search-input-example.tsx
@@ -1,13 +1,7 @@
 export default function SearchInputExample() {
   return (
-    <div className="di-flex di-gap-2 di-gray-bg di-p-2">
+    <div className="di-flex di-gap-2 di-bg-gray di-p-2">
       <div className="di-form-group" style={{ width: "200px" }}>
-        <span
-          className={`di-icon di-icon-search di-icon-24 di-bg-gray`}
-          role="img"
-          aria-label="Search Icon"
-        ></span>
-
         <input
           type="text"
           id="search-text-field-1"
@@ -15,7 +9,12 @@ export default function SearchInputExample() {
           aria-describedby="some-text-field-111"
           className="di-search-input"
         />
-        <label htmlFor="search-text-field-1" className="di-search-label">
+        <label htmlFor="search-text-field-1" className="di-search-label di-flex di-justify-center">
+          <span
+            className={`di-icon di-icon-search di-icon-24 di-bg-gray`}
+            role="img"
+            aria-label="Search Icon"
+          ></span>{" "}
           Label
         </label>
 
