9f46e4caab24775cf17b170d7cf89687d8728230
Stanislav Dolgachov <s.dolgachov@gmail.com>
2025-05-09 11:02:49 +0300

    feat: place search icon permanently

--- packages/docs/app/docs/components/form/form-search-input-example.tsx
+++ packages/docs/app/docs/components/form/form-search-input-example.tsx
@@ -2,6 +2,11 @@ export default function SearchInputExample() {
   return (
     <div className="di-flex di-gap-2 di-bg-gray di-p-2">
       <div className="di-form-group" style={{ width: "200px" }}>
+        <span
+          className="di-search-input-icon di-icon di-icon-24 di-icon-magnify-glass di-bg-gray"
+          role="img"
+          aria-label="Search Icon"
+        ></span>
         <input
           type="text"
           id="search-text-field-1"
@@ -9,12 +14,10 @@ export default function SearchInputExample() {
           aria-describedby="some-text-field-111"
           className="di-search-input"
         />
-        <label htmlFor="search-text-field-1" className="di-search-label di-flex di-justify-center">
-          <span
-            className={`di-icon di-icon-search di-icon-24 di-bg-gray`}
-            role="img"
-            aria-label="Search Icon"
-          ></span>{" "}
+        <label
+          htmlFor="search-text-field-1"
+          className="di-search-label di-flex di-justify-center"
+        >
           Label
         </label>
 
