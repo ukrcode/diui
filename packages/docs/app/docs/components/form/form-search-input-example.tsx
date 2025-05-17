b57438b3adeb672a8107975498d3de14bb0a335e
Stanislav Dolgachov <s.dolgachov@gmail.com>
2025-05-17 17:49:34 +0300

    feat: update search input with reverse icon

--- packages/docs/app/docs/components/form/form-search-input-example.tsx
+++ packages/docs/app/docs/components/form/form-search-input-example.tsx
@@ -1,16 +1,29 @@
 type SearchInputProps = {
   id: string;
-  bgClassName?: string;
+  rootClassName?: string;
   disabled?: boolean;
   isInvalidHint?: boolean;
+
+  isReverse?: boolean;
 };
 
-function SearchInput({ id, bgClassName, disabled, isInvalidHint }: SearchInputProps) {
+function SearchInput({
+  id,
+  rootClassName,
+  disabled,
+  isInvalidHint,
+  isReverse,
+}: SearchInputProps) {
   return (
-    <div className={`di-flex di-flex-wrap di-gap-2 di-p-2 ${bgClassName}`}>
-      <div className="di-form-group" style={{ width: "200px" }}>
+    <div className={`di-flex di-flex-wrap di-gap-2 di-p-2 ${rootClassName}`}>
+      <div
+        className={`di-form-group ${
+          isReverse ? "di-form-group-search-reverse" : "di-form-group-search"
+        }`}
+        style={{ width: "200px" }}
+      >
         <span
-          className="di-search-input-icon di-icon di-icon-24 di-icon-magnify-glass di-bg-gray"
+          className={`di-search-input-icon di-icon di-icon-24 di-icon-magnify-glass di-bg-gray`}
           role="img"
           aria-label="Search Icon"
         ></span>
@@ -20,16 +33,21 @@ function SearchInput({ id, bgClassName, disabled, isInvalidHint }: SearchInputPr
           disabled={disabled}
           placeholder=""
           aria-describedby="some-text-field-111"
-          className="di-search-input"
+          className="di-input"
         />
         <label
           htmlFor={id}
-          className="di-search-label di-flex di-flex-wrap di-justify-center"
+          className="di-label di-flex di-flex-wrap di-justify-center"
         >
           Label
         </label>
 
-        <div id={`${id}-hint`} className={`di-form-hint ${isInvalidHint ? "di-form-hint-invalid" : ""}`}>
+        <div
+          id={`${id}-hint`}
+          className={`di-form-hint ${
+            isInvalidHint ? "di-form-hint-invalid" : ""
+          }`}
+        >
           Hint Text
         </div>
       </div>
@@ -40,7 +58,12 @@ function SearchInput({ id, bgClassName, disabled, isInvalidHint }: SearchInputPr
 export default function SearchInputExample() {
   return (
     <>
-      <SearchInput id="some-text-field-010101" bgClassName="di-bg-gray" />
+      <SearchInput id="some-text-field-010101" rootClassName="di-bg-gray" />
+      <SearchInput
+        id="some-text-field-010103"
+        rootClassName="di-bg-gray"
+        isReverse
+      />
       <SearchInput id="some-text-field-020102" disabled isInvalidHint />
     </>
   );
