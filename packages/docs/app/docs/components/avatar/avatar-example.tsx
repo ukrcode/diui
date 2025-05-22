7e8c53127d12144ac89c04854b2fe11bf18966ca
Stanislav Dolgachov <s.dolgachov@gmail.com>
2025-05-22 18:05:12 +0300

    feat: add basic avatar + example

---
+++ packages/docs/app/docs/components/avatar/avatar-example.tsx
@@ -0,0 +1,8 @@
+export default function BadgeExample() {
+    return (
+      <div className="di-flex di-flex-wrap di-flex-col di-gap-2">
+        <div className="di-avatar">PP</div>
+      </div>
+    );
+  }
+  
