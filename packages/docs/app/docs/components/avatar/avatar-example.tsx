6350022bf0e1d1e26ead2fb8d607abccfe3f5968
Stanislav Dolgachov <s.dolgachov@gmail.com>
2025-05-23 12:32:37 +0300

    feat: add avatar sizes and square

--- packages/docs/app/docs/components/avatar/avatar-example.tsx
+++ packages/docs/app/docs/components/avatar/avatar-example.tsx
@@ -1,8 +1,23 @@
 export default function BadgeExample() {
-    return (
-      <div className="di-flex di-flex-wrap di-flex-col di-gap-2">
+  return (
+    <>
+      <div className="di-flex di-flex-align-center di-gap-2 di-mb-2">
+        <div className="di-avatar di-avatar-xl">PP</div>
+        <div className="di-avatar di-avatar-lg">PP</div>
         <div className="di-avatar">PP</div>
+        <div className="di-avatar di-avatar-md">PP</div>
+        <div className="di-avatar di-avatar-sm">PP</div>
+        <div className="di-avatar di-avatar-xs">PP</div>
       </div>
-    );
-  }
-  
+
+      <div className="di-flex di-flex-align-center di-gap-2 di-mb-2">
+        <div className="di-avatar di-avatar-square di-avatar-xl">PP</div>
+        <div className="di-avatar di-avatar-square di-avatar-lg">PP</div>
+        <div className="di-avatar di-avatar-square">PP</div>
+        <div className="di-avatar di-avatar-square di-avatar-md">PP</div>
+        <div className="di-avatar di-avatar-square di-avatar-sm">PP</div>
+        <div className="di-avatar di-avatar-square di-avatar-xs">PP</div>
+      </div>
+    </>
+  );
+}
