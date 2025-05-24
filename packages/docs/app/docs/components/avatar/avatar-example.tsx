fe49b98b396006899d1679c04be4929e07043445
Stanislav Dolgachov <s.dolgachov@gmail.com>
2025-05-24 16:53:12 +0300

    feat: add rounded square avatars

--- packages/docs/app/docs/components/avatar/avatar-example.tsx
+++ packages/docs/app/docs/components/avatar/avatar-example.tsx
@@ -18,6 +18,15 @@ export default function BadgeExample() {
         <div className="di-avatar di-avatar-square di-avatar-sm">PP</div>
         <div className="di-avatar di-avatar-square di-avatar-xs">PP</div>
       </div>
+
+      <div className="di-flex di-flex-align-center di-gap-2 di-mb-2">
+        <div className="di-avatar di-avatar-r-square di-avatar-xl">PP</div>
+        <div className="di-avatar di-avatar-r-square di-avatar-lg">PP</div>
+        <div className="di-avatar di-avatar-r-square">PP</div>
+        <div className="di-avatar di-avatar-r-square di-avatar-md">PP</div>
+        <div className="di-avatar di-avatar-r-square di-avatar-sm">PP</div>
+        <div className="di-avatar di-avatar-r-square di-avatar-xs">PP</div>
+      </div>
     </>
   );
 }
