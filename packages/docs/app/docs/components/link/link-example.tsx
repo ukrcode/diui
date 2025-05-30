af4ba48c4fe57f9afdab08cba9783bcb6274822c
Stanislav Dolgachov <s.dolgachov@gmail.com>
2025-05-30 10:53:38 +0300

    feat: add link types

---
+++ packages/docs/app/docs/components/link/link-example.tsx
@@ -0,0 +1,22 @@
+
+export default function LinkExample() {
+  return (
+    <>
+      <div className="di-p-2">
+        <a className="di-link"  href="#">Simple Link</a>
+      </div>
+
+      <div className="di-p-2 di-bg-black">
+        <a className="di-link di-link-inverted"  href="#">Simple Inverted Link</a>
+      </div>
+
+      <div className="di-p-2">
+        <a className="di-link di-link-primary" href="#">Primary Link</a>
+      </div>
+
+      <div className="di-p-2">
+        <a className="di-link di-link-secondary" href="#">Secondary Link</a>
+      </div>
+    </>
+  );
+}
