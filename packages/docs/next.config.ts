f4ad3033d9406f0e9ef272c1dfc9bc44d19d53f4
Stanislav Dolgachov <s.dolgachov@gmail.com>
2025-06-05 14:21:50 +0300

    ci/cd: add stylint to lib

--- packages/docs/next.config.ts
+++ packages/docs/next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   output: "export",
+  images: { unoptimized: true },
   // Optionally, add any other Next.js config below
 };
 
