730950c55c0aac55cef9b78f527f9d9b29dbb259
Stanislav Dolgachov <s.dolgachov@gmail.com>
2025-07-08 20:30:35 +0300

    ci/cd: add custom domain for docs

--- packages/docsV2/astro.config.mjs
+++ packages/docsV2/astro.config.mjs
@@ -7,7 +7,8 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://github.com/ukrcode",
-  base: "/diui",
+  site: "https://diui.org",
   integrations: [mdx(), preact()],
+  output: "static",
+  trailingSlash: "ignore",
 });
